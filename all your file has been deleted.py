2433e4d61b5236c8c40c2d1cfebd4d1cd3a7a5f6
J-scripting <110611803+J-scripting@users.noreply.github.com>
2022-09-22 20:13:50 +0800

    Add files via upload

---
+++ all your file has been deleted.py
@@ -0,0 +1,28 @@
+# A tool by nitro from www.noob-hackers.com
+# Be a unique person not copy cat
+# Colours
+#-----------------
+red="\e[91m"
+rset="\e[0m"
+grn="\e[92m"
+ylo="\e[93m"
+blue="\e[94m"
+cyan="\e[96m"
+pink="\e[95m"
+#-----------------
+# scripting start
+banner(){
+vid
+echo -e '\e[93m               
+                         
+                                    
+ALL YOUR FILES HAS BEEN DELETED AND ALSO YOUR STORAGE!
+
+
+                                                         
+                                                                               
+              
+'
+echo " " 
+echo -e "$red                       ▶Coded by$grn J-SCRIPTING$red◀$rset"
+echo -e "$red                      ⫸$ylo  J-SCRIPTING$red ⫷$rset"
